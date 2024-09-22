33ee2201950b98647741ab454fb8e1bc08d667fb
Audhav Durai <audhavdurai@lev-00230.apn.wlan.private.upenn.edu>
2024-09-22 00:59:16 -0400

    Changes

---
+++ pages/ocr.py
@@ -0,0 +1,152 @@
+import argparse
+import os
+
+import cv2
+import imutils
+import numpy as np
+import pytesseract
+from imutils.perspective import four_point_transform
+from cerebras_models import categorize_expenses
+import json 
+
+def perform_ocr(img: np.ndarray):
+    img_orig = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    image = img_orig.copy()
+    image = imutils.resize(image, width=500)
+    ratio = img_orig.shape[1] / float(image.shape[1])
+
+    # convert the image to grayscale, blur it slightly, and then apply
+    # edge detection
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    blurred = cv2.GaussianBlur(
+        gray,
+        (
+            5,
+            5,
+        ),
+        0,
+    )
+    edged = cv2.Canny(blurred, 75, 200)
+
+    # find contours in the edge map and sort them by size in descending
+    # order
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+    # initialize a contour that corresponds to the receipt outline
+    receiptCnt = None
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points, then we can
+        # assume we have found the outline of the receipt
+        if len(approx) == 4:
+            receiptCnt = approx
+            break
+
+    # if the receipt contour is empty then our script could not find the
+    # outline and we should be notified
+    if receiptCnt is None:
+        raise Exception(
+            (
+                "Could not find receipt outline. "
+                "Try debugging your edge detection and contour steps."
+            )
+        )
+
+    # apply a four-point perspective transform to the *original* image to
+    # obtain a top-down bird's-eye view of the receipt
+    receipt = four_point_transform(img_orig, receiptCnt.reshape(4, 2) * ratio)
+
+    # apply OCR to the receipt image by assuming column data, ensuring
+    # the text is *concatenated across the row* (additionally, for your
+    # own images you may need to apply additional processing to cleanup
+    # the image, including resizing, thresholding, etc.)
+    options = "--psm 6"
+    text = pytesseract.image_to_string(
+        cv2.cvtColor(receipt, cv2.COLOR_BGR2RGB), config=options
+    )
+    return text
+
+
+def getText(image_path="receipt.jpg"):
+    img_orig = cv2.imread(image_path)
+    image = img_orig.copy()
+    image = imutils.resize(image, width=500)
+    ratio = img_orig.shape[1] / float(image.shape[1])
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(
+        gray,
+        (
+            5,
+            5,
+        ),
+        0,
+    )
+    edged = cv2.Canny(blurred, 75, 200)
+
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+    # initialize a contour that corresponds to the receipt outline
+    receiptCnt = None
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points, then we can
+        # assume we have found the outline of the receipt
+        if len(approx) == 4:
+            receiptCnt = approx
+            break
+
+    # cv2.drawContours(image, [receiptCnt], -1, (0, 255, 0), 2)
+    # cv2.imwrite('image_with_outline.jpg', image)
+    # cv2.imshow("Receipt Outline", image)
+    # cv2.waitKey(0)
+
+    # if the receipt contour is empty then our script could not find the
+    # outline and we should be notified
+    if receiptCnt is None:
+        raise Exception(
+            (
+                "Could not find receipt outline. "
+                "Try debugging your edge detection and contour steps."
+            )
+        )
+
+    # apply a four-point perspective transform to the *original* image to
+    # obtain a top-down bird's-eye view of the receipt
+    receipt = four_point_transform(img_orig, receiptCnt.reshape(4, 2) * ratio)
+    # cv2.imwrite('transformed_receipt.jpg', receipt)
+
+    # apply OCR to the receipt image by assuming column data, ensuring
+    # the text is *concatenated across the row* (additionally, for your
+    # own images you may need to apply additional processing to cleanup
+    # the image, including resizing, thresholding, etc.)
+    options = "--psm 6"
+    text = pytesseract.image_to_string(
+        cv2.cvtColor(receipt, cv2.COLOR_BGR2RGB), config=options
+    )
+    # show the raw output of the OCR process
+    return text
+
+def getJson(image_path="receipt.jpg"): 
+    text = getText(image_path)
+    response = categorize_expenses(text)
+    content = str(response.choices[0].message.content)
+
+    # print("\n\n\n\n\n")
+    # print(content)
+    # print("\n\n\n\n\n")
+
+    json_data = json.loads(content)
+
+    # print(json_data)
